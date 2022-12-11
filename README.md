6e64c09e51e912eb6a7cdc28358ba81fcfce937c
hendikaseptio <hendikaseptio11@gmail.com>
2022-12-11 10:23:37 +0700

    memperbaiki halaman home

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Lapor Desaku
 
 Lapor Desaku adalah aplikasi untuk menyampaikan pengaduan secara online.
+
+# Design Prototype Aplikasi
+![The San Juan Mountains are beautiful!](prototype.png "San Juan Mountains")
--- lib/home.dart
+++ lib/home.dart
@@ -32,101 +32,110 @@ class LaporanSaya extends StatelessWidget {
           backgroundColor: Colors.purple,
         ),
         body: SafeArea(
-            minimum: const EdgeInsets.all(16.0),
-            child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Container(
+          minimum: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                Card(
+                  clipBehavior: Clip.antiAlias,
                   child: Column(
                     children: [
-                      Card(
-                        clipBehavior: Clip.antiAlias,
-                        child: Column(
-                          children: [
-                            Image.asset('sample/jalan.jpg'),
-                            ListTile(
-                              title: const Text('Jalan di perbatasan rusak'),
-                              subtitle: Text(
-                                '12-12-2022',
-                                style: TextStyle(
-                                    color: Colors.black.withOpacity(0.6)),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Text(
-                                'Mohon kepada kepala desa untuk segera menindak lanjuti laporan pengaduan ini karena pada lokasi tersebut sering terjadi kecelakaan',
-                                style: TextStyle(
-                                    color: Colors.black.withOpacity(0.6)),
-                              ),
-                            ),
-                            ButtonBar(
-                              alignment: MainAxisAlignment.end,
-                              children: [
-                                ElevatedButton.icon(
-                                  onPressed: () {
-                                    // Respond to button press
-                                  },
-                                  icon: Icon(Icons.edit_outlined, size: 18),
-                                  label: Text("Edit"),
-                                ),
-                                TextButton.icon(
-                                  onPressed: () {
-                                    // Respond to button press
-                                  },
-                                  icon: Icon(Icons.delete_outline_outlined, size: 18),
-                                  label: Text("Hapus"),
-                                ),
-                              ],
-                            ),
-                          ],
+                      Image.asset('sample/jalan.jpg'),
+                      ListTile(
+                        title: const Text('Jalan di perbatasan rusak'),
+                        subtitle: Text(
+                          '12-12-2022',
+                          style:
+                              TextStyle(color: Colors.black.withOpacity(0.6)),
                         ),
                       ),
-                      Card(
-                        clipBehavior: Clip.antiAlias,
-                        child: Column(
-                          children: [
-                            Image.asset('sample/sampah.jpg'),
-                            ListTile(
-                              title: const Text('Jalan di perbatasan rusak'),
-                              subtitle: Text(
-                                '12-12-2022',
-                                style: TextStyle(
-                                    color: Colors.black.withOpacity(0.6)),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Text(
-                                'Mohon kepada kepala desa untuk segera menindak lanjuti laporan pengaduan ini karena pada lokasi tersebut sering terjadi kecelakaan',
-                                style: TextStyle(
-                                    color: Colors.black.withOpacity(0.6)),
-                              ),
-                            ),
-                            ButtonBar(
-                              alignment: MainAxisAlignment.end,
-                              children: [
-                                ElevatedButton.icon(
-                                  onPressed: () {
-                                    // Respond to button press
-                                  },
-                                  icon: Icon(Icons.edit_outlined, size: 18),
-                                  label: Text("Edit"),
-                                ),
-                                TextButton.icon(
-                                  onPressed: () {
-                                    // Perform some action
-                                  },
-                                  icon: Icon(Icons.delete_outline_outlined, size: 18),
-                                  label: Text('Hapus'),
-                                ),
-                              ],
-                            ),
-                          ],
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          'Mohon kepada kepala desa untuk segera menindak lanjuti laporan pengaduan ini karena pada lokasi tersebut sering terjadi kecelakaan',
+                          style:
+                              TextStyle(color: Colors.black.withOpacity(0.6)),
                         ),
+                      ),
+                      ButtonBar(
+                        alignment: MainAxisAlignment.end,
+                        children: [
+                          ElevatedButton.icon(
+                            onPressed: () {
+                              // Respond to button press
+                            },
+                            icon: Icon(Icons.edit_outlined, size: 18),
+                            label: Text("Edit"),
+                          ),
+                          TextButton.icon(
+                            onPressed: () {
+                              // Respond to button press
+                            },
+                            icon: Icon(Icons.delete_outline_outlined, size: 18),
+                            label: Text("Hapus"),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ))),
+                ),
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  child: Column(
+                    children: [
+                      Image.asset('sample/sampah.jpg'),
+                      ListTile(
+                        title: const Text('Jalan di perbatasan rusak'),
+                        subtitle: Text(
+                          '12-12-2022',
+                          style:
+                              TextStyle(color: Colors.black.withOpacity(0.6)),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          'Mohon kepada kepala desa untuk segera menindak lanjuti laporan pengaduan ini karena pada lokasi tersebut sering terjadi kecelakaan',
+                          style:
+                              TextStyle(color: Colors.black.withOpacity(0.6)),
+                        ),
+                      ),
+                      ButtonBar(
+                        alignment: MainAxisAlignment.end,
+                        children: [
+                          ElevatedButton.icon(
+                            onPressed: () {
+                              // Respond to button press
+                            },
+                            icon: Icon(Icons.edit_outlined, size: 18),
+                            label: Text("Edit"),
+                          ),
+                          TextButton.icon(
+                            onPressed: () {
+                              // Perform some action
+                            },
+                            icon: Icon(Icons.delete_outline_outlined, size: 18),
+                            label: Text('Hapus'),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.purple,
+          foregroundColor: Colors.white,
+          mini: true,
+          onPressed: () {
+            // Respond to button press
+          },
+          child: Icon(Icons.add),
+        ),
         bottomNavigationBar: BottomNavigationBar(
           type: BottomNavigationBarType.fixed,
           // backgroundColor: Color.fromARGB(255, 124, 41, 241),
