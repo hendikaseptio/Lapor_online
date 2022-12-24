a28d836d35f2c2d030b0ab288c9d717b0ce6d2d8
hendikaseptio <hendikaseptio11@gmail.com>
2022-12-24 14:21:50 +0700

    memfungsikan bottom navigation bar

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'home.dart';
 class Login extends StatelessWidget {
   const Login({super.key});
 
@@ -20,118 +20,126 @@ class FormLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      minimum: const EdgeInsets.all(16.0),
-      child: Center(
+      body: SafeArea(
+        minimum: const EdgeInsets.all(16.0),
+        child: Center(
           child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Image.asset('images/logo.png', width: 200, height: 200),
-          Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40.0),
-              child: Column(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        icon: Icon(Icons.email_outlined),
-                        hintText: 'Masukkan Alamat Email',
-                        labelText: 'Email',
-                        // border: OutlineInputBorder(),
-                      ),
-                      onSaved: (String? value) {
-                        // This optional block of code can be used to run
-                        // code when the user saves the form.
-                      },
-                      validator: (String? value) {
-                        return (value != null && value.contains('@'))
-                            ? 'Do not use the @ char.'
-                            : null;
-                      },
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        icon: Icon(Icons.lock_outline),
-                        hintText: 'Masukkan Password',
-                        labelText: 'Password',
-                        // border: OutlineInputBorder(),
-                      ),
-                      obscureText: true,
-                      enableSuggestions: false,
-                      autocorrect: false,
-                      onSaved: (String? value) {
-                        // This optional block of code can be used to run
-                        // code when the user saves the form.
-                      },
-                      validator: (String? value) {
-                        return (value != null && value.contains('@'))
-                            ? 'Do not use the @ char.'
-                            : null;
-                      },
-                    ),
-                  ),
-                ],
-              )),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 40.0),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: SizedBox(
-                      height: 40,
-                      child: ElevatedButton.icon(
-                        onPressed: () {
-                          // Respond to button press
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Image.asset('assets/images/logo.png', width: 200, height: 200),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40.0),
+                child: Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(vertical: 10.0),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          icon: Icon(Icons.email_outlined),
+                          hintText: 'Masukkan Alamat Email',
+                          labelText: 'Email',
+                          // border: OutlineInputBorder(),
+                        ),
+                        onSaved: (String? value) {
+                          // This optional block of code can be used to run
+                          // code when the user saves the form.
                         },
-                        icon: Icon(Icons.login_outlined, size: 18),
-                        label: Text("Login"),
-                        style: ButtonStyle(
-                          overlayColor:
-                              MaterialStateProperty.resolveWith<Color?>(
+                        validator: (String? value) {
+                          return (value != null && value.contains('@'))
+                              ? 'Do not use the @ char.'
+                              : null;
+                        },
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(vertical: 10.0),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          icon: Icon(Icons.lock_outline),
+                          hintText: 'Masukkan Password',
+                          labelText: 'Password',
+                          // border: OutlineInputBorder(),
+                        ),
+                        obscureText: true,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        onSaved: (String? value) {
+                          // This optional block of code can be used to run
+                          // code when the user saves the form.
+                        },
+                        validator: (String? value) {
+                          return (value != null && value.contains('@'))
+                              ? 'Do not use the @ char.'
+                              : null;
+                        },
+                      ),
+                    ),
+                  ],
+                )
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        height: 40,
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => const Home(),
+                              ),
+                            );
+                          },
+                          icon: Icon(Icons.login_outlined, size: 18),
+                          label: Text("Login"),
+                          style: ButtonStyle(
+                            overlayColor:
+                            MaterialStateProperty.resolveWith<Color?>(
                               (Set<MaterialState> states) {
-                              if (states.contains(MaterialState.pressed))
-                                return Colors.purpleAccent; //<-- SEE HERE
-                              return null; // Defer to the widget's default.
-                            },
+                                if (states.contains(MaterialState.pressed))
+                                  return Colors.purpleAccent; //<-- SEE HERE
+                                return null; // Defer to the widget's default.
+                              },
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Expanded(
-                    child: SizedBox(
-                      height: 40,
-                      child: OutlinedButton.icon(
-                        onPressed: () {
-                          // Respond to button press
-                        },
-                        icon: Icon(Icons.mail_outline, size: 18),
-                        label: Text("Login Dengan Gmail"),
-                        style: ButtonStyle(
-                          overlayColor:
-                              MaterialStateProperty.resolveWith<Color?>(
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: SizedBox(
+                        height: 40,
+                        child: OutlinedButton.icon(
+                          onPressed: () {
+                            // Respond to button press
+                          },
+                          icon: Icon(Icons.mail_outline, size: 18),
+                          label: Text("Login Dengan Gmail"),
+                          style: ButtonStyle(
+                            overlayColor:
+                            MaterialStateProperty.resolveWith<Color?>(
                               (Set<MaterialState> states) {
-                              if (states.contains(MaterialState.pressed))
-                                return Colors.purpleAccent; //<-- SEE HERE
-                              return null; // Defer to the widget's default.
-                            },
+                                if (states.contains(MaterialState.pressed))
+                                  return Colors.purpleAccent; //<-- SEE HERE
+                                return null; // Defer to the widget's default.
+                              },
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ]),
-          ),
-        ],
-      )),
-    ));
+                  ]
+                ),
+              ),
+            ],
+          )
+        ),
+      )
+    );
   }
 }
